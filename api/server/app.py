61373885c3582d407bbf8e1234e23d399995c391
strange <38800238+rivasjoaquin02@users.noreply.github.com>
2023-07-11 20:49:30 -0400

    added hability of - create problems - view all problems

--- api/server/app.py
+++ api/server/app.py
@@ -1,9 +1,11 @@
 
 from fastapi import FastAPI
 from server.routes.auth import router as AuthRouter
+from server.routes.problems import router as ProblemsRouter
 
 app = FastAPI()
 app.include_router(AuthRouter, tags=["Auth"])
+app.include_router(ProblemsRouter, tags=["Problems"])
 
 
 @app.get("/", tags=["Root"])
