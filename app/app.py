34edbbf5d5c401adc2d220ebc9e18bd0c98088b8
strange <rivasjoaquin02@gmail.com>
2023-08-05 21:44:39 -0400

    added new env variable

--- app/app.py
+++ app/app.py
@@ -3,14 +3,19 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.routes.auth import router as AuthRouter
 from app.routes.users import router as UsersRouter
 from app.routes.problems import router as ProblemsRouter
+from dotenv import dotenv_values
+
+env = dotenv_values(".env")
 
 app = FastAPI()
 app.include_router(AuthRouter, tags=["Auth"], prefix="/auth")
 app.include_router(UsersRouter, tags=["Users"], prefix="/users")
 app.include_router(ProblemsRouter, tags=["Problems"], prefix="/problems")
 
+
 # CROSS (Cross-Origin RESOURCE SHARING)
-origins = ["http://localhost", "http://localhost:3000"]
+FRONTEND = str(env.get("FRONTEND", "default"))
+origins = [FRONTEND]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
